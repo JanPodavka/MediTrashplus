fffacbae438f111e2a664d13e6cfcf19252236d8
Jan Podávka <jan.podavka@tul.cz>
2021-12-13 23:22:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+
+
+class LoginWindow(Screen):
+    pass
+
+
+class MainWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+class MyApp(MDApp):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build(self):
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Blue"
+        screen = Builder.load_file("My.kv")
+        return screen
+
+
+if __name__ == '__main__':
+    MyApp().run()
