e2eaa08d77f503766a35d6662f30cc62c4bb9c22
Jan Podávka <jan.podavka@tul.cz>
2021-12-14 10:07:38 +0100

    funkční login, správa user...dodělat databázi

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
+from kivy.properties import StringProperty
 
 
 class LoginWindow(Screen):
-    pass
+
+    def login_user(self, loginText, passwordText):
+        app = MDApp.get_running_app()
+        app.usernameL = loginText
+        app.passwordL = passwordText
+        print(app.usernameL)
+        print(app.passwordL)
+        app.usernameL = self.ids['name'].text
+        app.passwordL = self.ids['password'].text
 
 
 class MainWindow(Screen):
@@ -24,6 +33,9 @@ class WindowManager(ScreenManager):
 class MyApp(MDApp):
 
     def build(self):
+        usernameL = StringProperty(None)
+        passwordL = StringProperty(None)
+
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Gray"
         screen = Builder.load_file("styly.kv")
