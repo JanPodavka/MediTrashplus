2f547e0f11d022deb60016f5a904e838312c267a
StazRos <anastazie.rosova@tul.com>
2021-12-14 13:50:35 +0100

    Safe

--- main.py
+++ main.py
@@ -9,28 +9,36 @@ from kivymd.app import MDApp
 from kivymd.uix.screen import Screen
 from kivy.properties import StringProperty
 
-
 class LoginWindow(Screen):
 
     def login_user(self, loginText, passwordText):
         app = MDApp.get_running_app()
-        app.usernameL = loginText
-        app.passwordL = passwordText
-        print(app.usernameL)
-        print(app.passwordL)
-        app.usernameL = self.ids['name'].text
-        app.passwordL = self.ids['password'].text
-
+        #Otevři soubor
+        with open('data/pass.txt') as f:
+            lines = f.readlines()
+        # porovnaj zda je v DTB (zatím txt)
+        for line in lines:
+            line = line.replace("\n", "")
+            udaje = line.split(" ")
+            if self.ids['name'].text == udaje[0] and self.ids['password'].text == udaje[1]:
+                print("úspěšně přihlášeno")
+                # Náhrání do údajů aktivnního uživatele
+                app.usernameL = loginText
+                app.passwordL = passwordText
+                # Vymazání text inputů
+                self.ids['name'].text = ""
+                self.ids['password'].text = ""
+                return True
+        print("Špatné údaje")
+        self.ids['name'].text = ""
+        self.ids['password'].text = ""
+        return False
 
 class MainWindow(Screen):
     pass
 
-class RegistrationWindow(Screen):
-    pass
-
 class WindowManager(ScreenManager):
     pass
-
 
 class MyApp(MDApp):
 
@@ -42,7 +50,6 @@ class MyApp(MDApp):
         self.theme_cls.primary_palette = "Gray"
         screen = Builder.load_file("styly.kv")
         return screen
-
 
 if __name__ == '__main__':
     MyApp().run()
