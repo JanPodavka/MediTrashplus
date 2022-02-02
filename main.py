d39bb5bce84c9d9ecb1019bbd49c198a220c8855
StazRos <anastazie.rosova@tul.cz>
2022-02-02 19:08:40 +0100

    PopUp - week danger

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from kivy.graphics import Line, Rectangle, Color
 from fpdf import FPDF
 from pathlib import Path
 from matplotlib import pyplot as plt
-from kivy.garden.matplotlib import FigureCanvasKivyAgg
+#from kivy.garden.matplotlib import FigureCanvasKivyAgg
 #pip3 install fpdf
 
 _ACCEPTED_BAR_CAPS = {"round", "none", "square"}
@@ -329,7 +329,7 @@ class StatisticWindow(Screen):
         plt.bar(nazvy, mnozstvi, color=(0, 0, 1, 0.6))
         plt.ylabel("Váha (g)")
         plt.xlabel("Kód odpadu")
-        self.ids['graf1'].add_widget(FigureCanvasKivyAgg(plt.gcf()))
+       # self.ids['graf1'].add_widget(FigureCanvasKivyAgg(plt.gcf()))
         plt.figure(4)
         SQL = "SELECT kategorie,SUM(mnozstvi) FROM 	Odpad,Katalog_odpadu WHERE katalogove_cislo = kod_odpadu AND odevezeno = ? AND zdravotnicke_zarizeni_ico = ? GROUP BY kategorie"
         val = ('ne', app.usernameL)
@@ -351,7 +351,7 @@ class StatisticWindow(Screen):
 
         plt.ylabel("Váha (g)")
         plt.xlabel("Kategorie")
-        self.ids['graf2'].add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        #self.ids['graf2'].add_widget(FigureCanvasKivyAgg(plt.gcf()))
 
     def first_graph(self,*args):
         self.ids['graf1'].clear_widgets()
@@ -371,7 +371,7 @@ class StatisticWindow(Screen):
         plt.bar(nazvy, mnozstvi, color=(0, 0, 1, 0.6))
         plt.ylabel("Váha (g)")
         plt.xlabel("Kód odpadu")
-        self.ids['graf1'].add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        #self.ids['graf1'].add_widget(FigureCanvasKivyAgg(plt.gcf()))
         plt.figure(2)
         SQL = "SELECT kategorie,SUM(mnozstvi) FROM 	Odpad,Katalog_odpadu WHERE katalogove_cislo = kod_odpadu AND  zdravotnicke_zarizeni_ico = ?	GROUP BY kategorie"
         data2 = app.cursor.execute(SQL, val)
@@ -392,8 +392,7 @@ class StatisticWindow(Screen):
 
         plt.ylabel("Váha (g)")
         plt.xlabel("Kategorie")
-        self.ids['graf2'].add_widget(FigureCanvasKivyAgg(plt.gcf()))
-
+        #self.ids['graf2'].add_widget(FigureCanvasKivyAgg(plt.gcf()))
 
 class MainWindow(Screen):
 
